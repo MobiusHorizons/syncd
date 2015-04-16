e58f2ac0ee1016d40c94f26154746ffce39a11bf
Brian Cole <bec8@students.calvin.edu>
2015-04-15 22:13:46 -0400

    initial logging functions

---
+++ src/log.c
@@ -0,0 +1,25 @@
+#include "log.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+FILE *__logfile;
+
+void logging_init(const char *filename)
+{
+	__logfile = fopen(filename, "a");
+}
+
+void logging_log(const char *fmt, ...)
+{
+	fprintf(__logfile, "Error at %s, line %d: ", __FILE__, __LINE__);
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(__logfile, fmt, args);
+	va_end(args);
+}
+
+void logging_close()
+{
+	fclose(__logfile);
+}
---
+++ src/log.h
@@ -0,0 +1,3 @@
+void logging_init(const char *filename);
+void logging_log(const char *fmt, ...);
+void logging_close();
