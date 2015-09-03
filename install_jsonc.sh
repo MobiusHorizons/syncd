5f87b044f9baafbbdbbdb7a79896bcb7596f5331
Paul Martin <codemobius@gmail.com>
2015-09-03 14:15:37 -0400

    fixed CFLAGS for jsonc

--- install_jsonc.sh
+++ install_jsonc.sh
@@ -6,7 +6,7 @@ if [ ! -d "$HOME/jsonc/lib" ]; then
   tar -xzvf json-c-0.12-20140410.tar.gz
   cd json-c-json-c-0.12-20140410
   CC="clang"
-  export CFLAGS="-Wno-error=unused-but-set-variable"
+  export CFLAGS=""
   ./configure --prefix=$HOME/jsonc
   make
   make install
