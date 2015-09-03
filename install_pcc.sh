737f1b31251cce54b6e2abea749f08d33e89d68b
Paul Martin <codemobius@gmail.com>
2015-09-03 14:21:20 -0400

    updated install_pcc to install pcc_libs

--- install_pcc.sh
+++ install_pcc.sh
@@ -3,12 +3,22 @@ set -e
 if [ ! -d "$HOME/pcc/bin/pcc" ]; then
   cd ..
   wget ftp://pcc.ludd.ltu.se/pub/pcc-releases/pcc-1.1.0.tgz
+  wget ftp://pcc.ludd.ltu.se/pub/pcc-releases/pcc-libs-1.1.0.tgz
   tar -xzvf pcc-1.1.0.tgz
-  cd pcc-1.1.0
-  CC="clang"
-  ./configure --prefix=$HOME/pcc
-  make
-  make install
+  tar -xzvf pcc-libs-1.1.0.tgz
+  CC="gcc"
+  (
+	  cd pcc-1.1.0
+	  ./configure --prefix=$HOME/pcc
+	  make
+	  make install
+  )
+  (
+	  cd pcc-libs-1.1.0
+	  ./configure --prefix=$HOME/pcc
+	  make
+	  make install
+  )
 else
   echo 'Using cached copy of pcc.';
 fi
