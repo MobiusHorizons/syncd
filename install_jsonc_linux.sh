b4e01b8ea7653add0d239d8547d6e04a3c1e4c79
Paul Martin <paul.martin@cqlcorp.com>
2015-10-08 11:42:34 -0400

    first attempt at travis building on osx and linux

---
+++ install_jsonc_linux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+if [ ! -d "$HOME/jsonc_linux/lib" ]; then
+  cd ..
+  wget https://github.com/json-c/json-c/archive/json-c-0.12-20140410.tar.gz
+  tar -xzvf json-c-0.12-20140410.tar.gz
+  cd json-c-json-c-0.12-20140410
+  CC="clang"
+  export CCFLAGS="-Wno-error=unused-but-set-variable"
+  ./configure --prefix=$HOME/jsonc_linux
+  make
+  make install
+else
+  echo 'Using cached copy of json-c.';
+fi
--- install_jsonc_osx.sh
+++ install_jsonc_osx.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
-if [ ! -d "$HOME/jsonc/lib" ]; then
+if [ ! -d "$HOME/jsonc_osx/lib" ]; then
   cd ..
   wget https://github.com/json-c/json-c/archive/json-c-0.12-20140410.tar.gz
-  tar -xzvf json-c-0.12-20140410.tar.gz
+  tar -xvf json-c-0.12-20140410.tar.gz
   cd json-c-json-c-0.12-20140410
   CC="clang"
   export CCFLAGS="-Wno-error=unused-but-set-variable"
-  ./configure --prefix=$HOME/jsonc
+  ./configure --prefix=$HOME/jsonc_osx
   make
   make install
 else
